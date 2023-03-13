9450a5da15fe4bda107ae4146003e0d2c4c3adfe
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-03-13 11:15:24 +0600

    toasts css aplited

--- src/components/AToast/Toasts.vue
+++ src/components/AToast/Toasts.vue
@@ -56,6 +56,7 @@ function endAnimation(toast: any) {
           class="p-relative"
         >
           <div
+            class="a-toast-type"
             style="margin: 16px 11px; position: relative"
             :class="{ [`a-${toast.type}`]: true }"
           >
@@ -66,7 +67,7 @@ function endAnimation(toast: any) {
             <div
               id="progress"
               ref="progress"
-              class="a-toast-progress"
+              class="a-toast__progress"
               :style="{
                 '--animation': `toastprogress ${toast.duration!! / 1000}s`
               }"
@@ -78,120 +79,3 @@ function endAnimation(toast: any) {
     </div>
   </teleport>
 </template>
-
-<style>
-.toasts,
-.toasts--top-right {
-  position: fixed;
-  top: 0;
-  right: 0;
-  z-index: 999999;
-}
-
-.toasts--bottom-right {
-  position: fixed;
-  top: auto;
-  bottom: 0;
-  right: 0;
-  left: auto;
-  z-index: 999999;
-}
-
-.toasts--top-left {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: auto;
-  z-index: 999999;
-}
-
-.toasts--bottom-left {
-  position: fixed;
-  top: auto;
-  bottom: 0;
-  left: 0;
-  right: auto;
-  z-index: 999999;
-}
-
-.toasts--top-center {
-  position: fixed;
-  top: 0;
-  bottom: auto;
-  left: 50%;
-  right: auto;
-  z-index: 999999;
-  transform: translateX(-50%);
-}
-
-.toasts--bottom-center {
-  position: fixed;
-  top: auto;
-  bottom: 0;
-  left: 50%;
-  right: auto;
-  z-index: 999999;
-  transform: translateX(-50%);
-}
-
-.slide-left-enter-active,
-.slide-right-enter-active,
-.slide-top-enter-active,
-.slide-bottom-enter-active,
-.slide-top-leave-active,
-.slide-bottom-leave-active,
-.slide-left-leave-active,
-.slide-right-leave-active {
-  transition: all 0.25s ease;
-}
-.slide-left-enter-from,
-.slide-left-leave-to {
-  opacity: 0;
-  transform: translateX(70px);
-}
-
-.slide-right-enter-from,
-.slide-right-leave-to {
-  opacity: 0;
-  transform: translateX(-70px);
-}
-
-.slide-bottom-enter-from,
-.slide-bottom-leave-to {
-  opacity: 0;
-  transform: translateY(-70px);
-}
-
-.slide-top-enter-from,
-.slide-top-leave-to {
-  opacity: 0;
-  transform: translateY(70px);
-}
-
-.a-toast-progress {
-  min-height: 1px;
-  background-color: var(--a-c-theme-600);
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 0%;
-  color: #fff;
-  animation: var(--animation);
-  animation-timing-function: linear;
-  animation-play-state: running;
-  z-index: 9999;
-}
-
-/* .a-toasts-container:hover .a-toast-progress {
-  animation-play-state: paused;
-} */
-
-@keyframes toastprogress {
-  from {
-    width: 100%;
-  }
-  to {
-    width: 0%;
-  }
-}
-</style>
